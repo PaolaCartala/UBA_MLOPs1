200ec8070bbdc9b18092da81a09f16bf9065caeb
Gastón Schvarchman <gaston.e@producteca.com>
2025-04-18 15:21:06 -0300

    modifico script dummy para probar mlflow

--- notebook/train_model.py
+++ notebook/train_model.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import os
 import boto3
 from io import BytesIO
 from sklearn.ensemble import RandomForestRegressor
@@ -6,7 +8,6 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import mlflow.sklearn
 
-# Configuración de MinIO
 s3 = boto3.client(
     "s3",
     endpoint_url="http://localhost:9000",
@@ -14,21 +15,24 @@ s3 = boto3.client(
     aws_secret_access_key="minio123"
 )
 
+os.environ["AWS_ACCESS_KEY_ID"] = "minio"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "minio123"
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://localhost:9000"
+
 def cargar_desde_minio(bucket, nombre_archivo):
     obj = s3.get_object(Bucket=bucket, Key=nombre_archivo)
     return pd.read_csv(BytesIO(obj['Body'].read()))
 
-# Cargar datasets
 train_df = cargar_desde_minio("data", "train.csv")
 test_df = cargar_desde_minio("data", "test.csv")
 
-# Separar features y target
-X_train = train_df.drop(columns=["ventas"])  # ajustá esto a tu target real
-y_train = train_df["ventas"]
-X_test = test_df.drop(columns=["ventas"])
-y_test = test_df["ventas"]
+X_train = train_df.drop(columns=["Ventas"])
+X_train = X_train.select_dtypes(include=["number"]) #lo agrego para que no rompa, pero en el modelo real hacer las conversiones necesarias
+y_train = train_df["Ventas"]
+X_test = test_df.drop(columns=["Ventas"])
+X_test = X_test.select_dtypes(include=["number"])
+y_test = test_df["Ventas"]
 
-# Configurar MLflow
 mlflow.set_tracking_uri("http://localhost:5000")
 mlflow.set_experiment("prediccion_ventas")
 
@@ -38,14 +42,13 @@ mlflow.set_experiment("prediccion_ventas")
 
 
 with mlflow.start_run(): ## acá hay que poner el modelo que utilicemos ↓
-    # Entrenamiento
+    
     n_estimators = 100
     model = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
     model.fit(X_train, y_train)
 
-    # Predicción y evaluación
     y_pred = model.predict(X_test)
-    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
     # Logging en MLflow. Si agregamos otros parámetros u otras métricas hay que agregarlas acá↓
     mlflow.log_param("n_estimators", n_estimators)
